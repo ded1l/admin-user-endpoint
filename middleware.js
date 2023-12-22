60c0a4e56f6b0b89583bef24378a3a8a8dc2da8d
ded1l <bbb0009@mtu.edu.iq>
2023-12-22 03:06:30 +0300

    test08

--- middleware.js
+++ middleware.js
@@ -2,20 +2,36 @@ const jwt = require("jsonwebtoken");
 
 async function AuthA(req, res, next) {
   const token = req.headers.token;
-  jwt.verify(token, "admin", function (err, decoded) {
-    if (decoded) {
-      next();
-    } else res.status(401).send({ success: false, msg: "Unauthorized!" });
-  });
+  try {
+    jwt.verify(token, process.env.POWER_KEY);
+    next();
+  } catch (err) {
+    res
+      .status(401)
+      .send({ success: false, msg: "Unauthorized!", error: err.message });
+  }
 }
 
 async function AuthU(req, res, next) {
-  let token = req.headers.token;
-  jwt.verify(token, "user", function (err, decoded) {
-    if (decoded) {
+  const token = req.header("Authorization");
+  if (!token) return res.status(401).send({ message: "Access Denied!" });
+  try {
+    jwt.verify(token, process.env.POWER_KEY);
+
+    next();
+  } catch (err) {
+    try {
+      jwt.verify(token, process.env.USER_KEY);
+
       next();
-    } else res.status(401).send({ success: false, msg: "Unauthorized!" });
-  });
+    } catch (err) {
+      res.status(401).send({ message: "Access Denied!" });
+      console.log(err);
+    }
+  }
 }
 
-module.exports = { AuthA, AuthU };
+module.exports = {
+  AuthA,
+  AuthU
+};
